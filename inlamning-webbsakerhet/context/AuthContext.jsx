756a27fcd950ab9df0f9acc0c2d7fdd588e759d9
Niklas Johansson <niklasdjohansson@gmail.com>
2022-10-26 20:10:37 +0200

    start context

---
+++ inlamning-webbsakerhet/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+require("dotenv").config();
+import { db, pool } from "../../api-express/database";
+
+export const AuthContext = createContext();
+
+export const AllContextProvider = ({ children }) => {
+  const [isWorker, setIsWorker] = useState(false);
+  const [isBoss, setIsBoss] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  console.log();
+
+  useEffect(() => {
+    db.getRoles = () => {
+      return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM Users WHERE role = ?";
+        pool.query(sql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    };
+    getUsers();
+    console.log(sql);
+  }, [sql]);
+
+  return (
+    <AllContext.Provider
+      value={{
+        isWorker,
+        setIsWorker,
+        isBoss,
+        setIsBoss,
+        isAdmin,
+        setIsAdmin,
+      }}
+    >
+      {children}
+    </AllContext.Provider>
+  );
+};
--- inlamning-webbsakerhet/pages/_app.tsx
+++ inlamning-webbsakerhet/pages/_app.tsx
@@ -1,9 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { AuthContext } from "../context/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return;
+  <AllContextProvider>
+    <Component {...pageProps} />
+  </AllContextProvider>;
 }
 
-export default MyApp
+export default MyApp;
